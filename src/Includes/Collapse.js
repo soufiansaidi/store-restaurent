b6d66f162e03e2cc6bb61255219af877b9a0a0ab
soufian <soufian@MacBook-Pro-de-soufian.local>
2020-04-29 14:59:32 +0000

    Add Order Mode page

--- src/Includes/Collapse.js
+++ src/Includes/Collapse.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 import { IoIosArrowDown } from "react-icons/io";
 
-export default function Collapse(title, {children}) {
+export default function Collapse({children}) {
     const [state, setstate] = useState(true)
 
     return (
         <div className={ (state) ? 'active collapse-data' : 'collapse-data' }>
             <div className="row">
-                <div className="collapse-title" onClick={ () => setstate(!state) }>{ title } <IoIosArrowDown /></div>
+                <div className="collapse-title" onClick={ () => setstate(!state) }>Beef burgers <IoIosArrowDown /></div>
             </div>
             <div className="collapse-content">{ children }</div>
         </div>
